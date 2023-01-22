1a17e306943ec38af4d5f9a076adb1853e106b3b
atemiks <atemiks@gmail.com>
2023-01-22 20:50:55 +0200

    Password verification

--- 06-basics-of-javascript/08-basics-of-javascript.js
+++ 06-basics-of-javascript/08-basics-of-javascript.js
@@ -1,3 +1,19 @@
 const userPassword = prompt(
     'Введите пароль. Пароль должен содержать минимум 3 символа, максимум 20, прописную букву и цифру'
 );
+
+const regExpUpperLetter = /[A-Z]/;
+const regExpNumber = /[0-9]/;
+
+if (
+    userPassword.length >= 3 &&
+    userPassword.length <= 20 &&
+    userPassword.match(regExpUpperLetter) &&
+    userPassword.match(regExpNumber)
+) {
+    alert('Пароль валидный. Добро пожаловать в аккаунт!');
+} else {
+    alert(
+        'Пароль не удовлетворяет условиям! Перезагрузите страницу и попробуйте ввести его еще раз.'
+    );
+}
